7976aaf7218ade1c3f1de631442f118bf961486d
Amrut-17 <68114007+Amrut-17@users.noreply.github.com>
2021-01-14 12:14:29 +0530

    Update PrimeNumbers.java
    Pass all test cases successfully.

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -3,27 +3,27 @@ import java.util.Scanner;
 public class PrimeNumbers {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int count = 0;
-
-        System.out.println("Enter Number to check wether its Prime number or not");
-        int number = s.nextInt();
-        int half = number / 2;
-
-        if(number == 0 || number == 1){
-            System.out.println("Number is not Prime number");
-        }
-        else{
-            for(int i=2; i<=half; i++){
-                if (number % i == 0){
-                    System.out.println(number + " NOT prime NUMBER");
-                    count += 1;
-                    break;
-                }
-            }
-        }
-
-        if (count == 0){
-            System.out.println(number + " is PRIME number");
+		int t = s.nextInt();
+        for(int j=0; j<t; j++){
+		    int n = s.nextInt();
+		    if(n>1){
+		    int count =0;
+		    for(int i=2; i<(int)Math.sqrt(n)+1; i++){
+		        if(n%i == 0){
+		            count += 1;
+		        }
+		    }
+		    if(count > 0){
+		        System.out.println("no");
+		    }
+		    else{
+		        System.out.println("yes");
+		    }
+		}
+		else{
+		    System.out.println("no");
+		}
         }
     }
+
 }
