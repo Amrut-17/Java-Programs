0631307e6cda7320a84467c0dcc3a37c26885663
_amrut_ <68114007+Amrut-17@users.noreply.github.com>
2021-05-17 18:03:25 +0530

    Create PalindromeLinkedList.java
    #Easy

---
+++ LeetCode/PalindromeLinkedList.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) return true;
+        
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        slow.next = reverseRightHalf(slow.next);
+        slow = slow.next;
+        
+        while(slow != null){
+            if(head.val != slow.val){
+                return false;
+            }
+            slow = slow.next;
+            head = head.next;
+        }
+        return true;
+    }
+    static ListNode reverseRightHalf(ListNode head){
+        ListNode prev = null;
+        ListNode next = null;
+        while(head != null){
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
