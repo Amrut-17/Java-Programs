4fd7685ba3cf261dd37d973ae512202315e4b784
_amrut_ <68114007+Amrut-17@users.noreply.github.com>
2021-09-02 07:01:58 -0700

    Create BuildArrayfromPermutation.java
    # Easy # Array 
    You have to transform each array element using a = r + bq formula. where q > r and b is not divisible by q. This is a trick used to do in-place array manupulation.
    r holds the prev num and bq holds the answer.

---
+++ LeetCode/BuildArrayfromPermutation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
+
+
+
+/*
+BEST APPROACH :
+
+public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        
+        for(int i=0; i<n; i++){
+            nums[i] = nums[i] + n*(nums[nums[i]] % n);
+        }
+        
+        for(int i=0; i<n; i++){
+            nums[i] = nums[i]/n;
+        }
+        
+        return nums;
+    }
+*/
